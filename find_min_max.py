6e7a10303dbd8471d910ea5acf5e82aa8c86f7ca
mohamed20medhat <egailmohamed2018@gmail.com>
2022-02-09 15:34:30 +0200

    added another method for min max

--- find_min_max.py
+++ find_min_max.py
@@ -2,7 +2,7 @@
 
 l = [1, 2, 3, 4, 5, 6, 7, 1, 3, 4, 8, 1]
 
-def find_smallest(arr):
+def find_min(arr):
     min = 1000000000000000000000
     for i in range(len(arr)):
         if min > arr[i]:
@@ -18,4 +18,23 @@ def find_max(arr):
     return max
 
 
+# another way 
+# notice that when using for num in nums. num is not an itterator. it's the actual value of the item of the array. not a loop varyable that has auto_increment or auto_decrement 
+def min_max_list(arr, min_max):
+    if min_max == "min" : 
+        min = 100000000000000000
+        for i in arr : 
+            if min > i: 
+                min = i
+        return min 
+    elif min_max == 'max' : 
+        max =  -10000000000000000000000000
+        for y in arr : 
+            if max < y: 
+                max = y
+        return max
+
+print (min_max_list(l,'max'))
+print (min_max_list(l,'min'))
 print(find_max(l))
+print(find_min(l))
